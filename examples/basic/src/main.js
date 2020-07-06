c8cf5b83e858b0b9579a2d88a268b88988501200
Allan Chain <allanchain@pku.edu.cn>
2020-07-06 17:26:45 +0800

    chore: comment for register in dev

--- examples/basic/src/main.js
+++ examples/basic/src/main.js
@@ -3,7 +3,8 @@
 
 import DefaultLayout from '~/layouts/Default.vue'
 
-export default function (Vue, { router, head, isClient }) {
+export default function (Vue, { isClient }) {
+  // Register on dev too for demo
   if (isClient) require('./registerServiceWorker')
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
