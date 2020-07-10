81e52f259d734805f9161fa929808f4da3e2c6dd
Allan Chain <allanchain@pku.edu.cn>
2020-07-10 21:40:59 +0800

    feat: throw error if options.icon not found

--- __tests__/manifest.unit.js
+++ __tests__/manifest.unit.js
@@ -1,6 +1,10 @@
+const path = require('path')
 const generateManifest = require('../lib/generateManifest')
 const { defaultOptions } = require('../gridsome.server')
 const defaultsDeep = require('lodash/defaultsDeep')
+
+const context = path.join(__dirname, '..', 'examples', 'basic')
+process.chdir(context)
 
 const manifest = userOptions => generateManifest({
   name: 'Awesome Gridsome',
@@ -15,7 +19,10 @@ describe('Generate Manifest', () => {
     expect(JSON.stringify(manifest({}))).not.toMatch('//')
   })
   it('gets correct icon type', () => {
-    expect(manifest({ icon: 'src/favicon.test.ico' }).icons[0].type)
-      .toBe('image/ico')
+    expect(manifest({}).icons[0].type).toBe('image/png')
+  })
+  it('throws error if icon not found', () => {
+    expect(() => manifest({ icon: 'src/not.exist.png' }))
+      .toThrow('not found')
   })
 })
