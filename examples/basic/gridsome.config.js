9088e3d16844e53d0c6800e6dcceae573ae97d3d
Allan Chain <allanchain@pku.edu.cn>
2020-07-25 22:20:29 +0800

    docs(example): simple InjectManifest example

--- examples/basic/gridsome.config.js
+++ examples/basic/gridsome.config.js
@@ -4,35 +4,48 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const options = {
+  default: {
+    manifestOptions: {
+      short_name: 'Gridsome',
+      description: 'Gridsome is awesome!',
+      display: 'standalone',
+      gcm_sender_id: undefined,
+      start_url: '/',
+      categories: ['education'],
+      lang: 'en-GB',
+      dir: 'auto'
+    },
+    appleMobileWebAppStatusBarStyle: 'default',
+    manifestPath: 'manifest.json',
+    icon: 'src/favicon.png',
+    msTileColor: '#00a672',
+    workboxOptions: {
+      cacheId: 'awesome-pwa',
+      skipWaiting: true,
+      exclude: [
+        /manifest\.json/
+      ]
+    }
+  },
+  InjectManifest: {
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: './src/service-worker.js',
+      additionalManifestEntries: [
+        '/index.html'
+      ]
+    }
+  }
+}
+
 module.exports = {
   siteName: 'Awesome Gridsome',
   pathPrefix: '/gridsome',
   plugins: [
     {
       use: '@allanchain/gridsome-plugin-pwa',
-      options: {
-        manifestOptions: {
-          short_name: 'Gridsome',
-          description: 'Gridsome is awesome!',
-          display: 'standalone',
-          gcm_sender_id: undefined,
-          start_url: '/',
-          categories: ['education'],
-          lang: 'en-GB',
-          dir: 'auto'
-        },
-        appleMobileWebAppStatusBarStyle: 'default',
-        manifestPath: 'manifest.json',
-        icon: 'src/favicon.png',
-        msTileColor: '#00a672',
-        workboxOptions: {
-          cacheId: 'awesome-pwa',
-          skipWaiting: true,
-          exclude: [
-            /manifest\.json/
-          ]
-        }
-      }
+      options: options[process.env.PWA_OPTIONS || 'default']
     }
   ]
 }
