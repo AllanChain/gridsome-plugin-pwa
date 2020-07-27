235436568a8978b70de87daf465aca0ae575197b
Allan Chain <allanchain@pku.edu.cn>
2020-07-27 17:53:16 +0800

    test(workbox): tests for new config generation

--- __tests__/workbox.unit.js
+++ __tests__/workbox.unit.js
@@ -2,10 +2,16 @@ const generateWorkboxConfig = require('../lib/generateWorkboxConfig')
 const { defaultOptions } = require('../gridsome.server')
 const defaultsDeep = require('lodash/defaultsDeep')
 
+const apiConfig = {
+  siteName: 'Awesome Gridsome',
+  outputDir: 'gridsome',
+  publicPath: 'gridsome/'
+}
+
 const workbox = userOptions => {
   const options = defaultsDeep(userOptions, defaultOptions())
   return generateWorkboxConfig(
-    'Awesome Gridsome',
+    apiConfig,
     options.workboxPluginMode,
     options.workboxOptions
   )
@@ -13,32 +19,31 @@ const workbox = userOptions => {
 
 describe('Generate Workbox Config', () => {
   it('works with zero config', () => {
-    const config = workbox({})
-    expect(config.cacheId).toBe('Awesome Gridsome')
-    expect(config.exclude.length).toBe(3)
+    const { workboxConfig, compileOptions } = workbox({})
+    expect(workboxConfig.cacheId).toBe('Awesome Gridsome')
+    expect(workboxConfig.swDest).toBe('gridsome/service-worker.js')
+    expect(compileOptions).toBe(false)
   })
   it('throws error on unknown mode', () => {
     expect(() => workbox({ workboxPluginMode: 'InhectManidfet' }))
       .toThrow('is not a supported')
   })
   it('takes config', () => {
-    expect(workbox({ workboxOptions: { skipWaiting: true } }).skipWaiting)
-      .toBeTruthy()
+    expect(
+      workbox({ workboxOptions: { skipWaiting: true } })
+        .workboxConfig.skipWaiting
+    ).toBeTruthy()
   })
-  it('should be able to use InjectManifest', () => {
-    const config = workbox({
-      workboxPluginMode: 'InjectManifest',
+  it('should be able to use injectManifest', () => {
+    const { workboxConfig, compileOptions } = workbox({
+      workboxPluginMode: 'injectManifest',
       workboxOptions: {
-        swSrc: 'src/sw.js'
+        swSrc: './src/sw.js',
+        swDest: 'gridsome/sw.js'
       }
     })
-    expect(config).toEqual({
-      swSrc: 'src/sw.js',
-      exclude: [
-        /styles(\.\w{8})?\.js$/,
-        /manifest\/client.json$/,
-        /assets\/icons/
-      ]
-    })
+    expect(workboxConfig.swSrc).toBe('gridsome/sw.js')
+    expect(compileOptions.swSrc).toBe('./src/sw.js')
+    expect(compileOptions.swDest).toBe('sw.js')
   })
 })
